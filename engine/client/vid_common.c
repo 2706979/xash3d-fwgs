8e57fc295d544077c99f1320c648ce7cc2cb7d82
2706979 <2706979862@qq.com>
2025-12-08 21:07:58 +0800

    Update vid_common.c

--- engine/client/vid_common.c
+++ engine/client/vid_common.c
@@ -1,6 +1,6 @@
 /*
 vid_common.c - common vid component
-Copyright (C) 2018 a1batross, Uncle Mike
+Copyright (C) 2018 a1batros, Uncle Mike
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
